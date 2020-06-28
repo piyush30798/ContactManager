b426dd8c6932ad51fdd5e749e168e2a069e412ef
piyush30798 <pdaga3@gmail.com>
2020-06-28 14:47:01 +0530

    hellow world

--- app/src/main/java/com/example/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/contactmanager/MainActivity.java
@@ -3,12 +3,115 @@ package com.example.contactmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt10,bt11,bt12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1=findViewById(R.id.bt1);
+        bt2=findViewById(R.id.bt2);
+        bt3=findViewById(R.id.bt3);
+        bt4=findViewById(R.id.bt4);
+        bt5=findViewById(R.id.bt5);
+        bt6=findViewById(R.id.bt6);
+        bt7=findViewById(R.id.bt7);
+        bt8=findViewById(R.id.bt8);
+        bt9=findViewById(R.id.bt9);
+        bt10=findViewById(R.id.bt10);
+        bt11=findViewById(R.id.bt11);
+        bt12=findViewById(R.id.bt12);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        bt12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
