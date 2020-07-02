98e9ef03903d875cf99e50585f1065ca2497dd58
piyush30798 <pdaga3@gmail.com>
2020-07-02 23:32:31 +0530

    hellow world

---
+++ app/src/main/java/com/example/contactmanager/FragmentContacts.java
@@ -0,0 +1,26 @@
+package com.example.contactmanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentContacts extends Fragment {
+
+    View view;
+
+    public FragmentContacts() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        view=inflater.inflate(R.layout.contacts_fragment,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/contactmanager/MainActivity.java
@@ -1,152 +1,45 @@
 package com.example.contactmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt10,bt11,bt12;
-    TextView txt;
+     private Toolbar toolb;
+     private TabLayout tabLayout;
+     private AppBarLayout appBarLayout;
+     private ViewPager viewPager;
 
-    String num="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bt1=findViewById(R.id.bt1);
-        bt2=findViewById(R.id.bt2);
-        bt3=findViewById(R.id.bt3);
-        bt4=findViewById(R.id.bt4);
-        bt5=findViewById(R.id.bt5);
-        bt6=findViewById(R.id.bt6);
-        bt7=findViewById(R.id.bt7);
-        bt8=findViewById(R.id.bt8);
-        bt9=findViewById(R.id.bt9);
-        bt10=findViewById(R.id.bt10);
-        bt11=findViewById(R.id.bt11);
-        bt12=findViewById(R.id.bt12);
-        txt=findViewById(R.id.txt);
+        toolb=findViewById(R.id.toolb);
+        setSupportActionBar(toolb);
+        tabLayout=findViewById(R.id.tabL);
+        appBarLayout=findViewById(R.id.appB);
+        viewPager=findViewById(R.id.viewP);
 
-
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "1";
-                txt.setText(num);
-
-            }
-        });
-
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "2";
-                txt.setText(num);
-
-            }
-        });
-
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "3";
-                txt.setText(num);
-            }
-        });
-
-        bt4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "4";
-                txt.setText(num);
-            }
-        });
-
-        bt5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "5";
-                txt.setText(num);
-            }
-        });
-
-        bt6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "6";
-                txt.setText(num);
-            }
-        });
-
-        bt7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "7";
-                txt.setText(num);
-            }
-        });
-
-        bt8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "8";
-                txt.setText(num);
-            }
-        });
-
-        bt9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "9";
-                txt.setText(num);
-            }
-        });
-
-        bt10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "*";
-                txt.setText(num);
-            }
-        });
-
-        bt11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "0";
-                txt.setText(num);
-            }
-        });
-
-
-        bt12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                num = num + "#";
-                txt.setText(num);
-
-            }
-        });
-
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new FragmentDial(),"DialPad");
+        adapter.AddFragment(new FragmentContacts(),"Contacts");
+        adapter.AddFragment(new FragmentNearBy(),"NearBy");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
+
 }
